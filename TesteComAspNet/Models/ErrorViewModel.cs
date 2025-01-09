0dee34bdd5f7be4f7b68a7ab9c2f631a1a9d45d9
Cristiano Castiel <cristianoct03@gmail.com>
2025-01-09 17:18:51 -0300

    TesteInicial

---
+++ TesteComAspNet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TesteComAspNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
